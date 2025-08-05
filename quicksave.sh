c12e7df4cb5739ebac43078ffe7b859649cc1059
root <jaywbattle@gmail.com>
2025-08-04 20:10:24 -0400

    all support added

--- quicksave.sh
+++ quicksave.sh
@@ -4,14 +4,31 @@
 # Requires: git, quickcommit.sh
 # Prequisites: authenticate w/ git Ex: (git config --global user.email "EMAIL") (add git ssh key)
 # Note: Do not use sudo!
-CommitMessage=$1
+Scope=$1
+CommitMessage=$2
+timestamp=$(date)
 
-for d in */ ; do
-    read -p "Would you like to commit $d? y or n: " -r response
-    if [ "$response" == "y" ]; then
+if [[ "$1" == "all" ]]; then
+    echo "Commiting All Repositories!"
+    for d in */ ; do
         echo "committing $d"
         cd $d
         quickcommit.sh \"$(date)\"
         cd ..
-    fi
+    done
+else
+    for d in */ ; do
+    read -p "Would you like to commit $d? y or n: " -r response
+    case "$response" in
+        "y" | "Y" )
+            echo "committing $d"
+            cd $d
+            quickcommit.sh \"$(date)\"
+            cd .. ;;
+        "n" | "N" )
+            echo "Skipping $d" ;;
+        * )
+            echo "Invalid input. Skipping $d" ;;
+    esac
 done
+fi
